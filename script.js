d4cea4e808e5783aa948ea82fdfce3daf43cfbbe
abythomas300 <abythomas300@gmail.com>
2025-06-07 12:03:58 +0530

    feat: add start button to start the game

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ let hasBlackjack = false;
 let isAlive = true;
 let message = '';
 
-// User age check
+
 let age = 25;
 
+
+// MEthod to check user age 
 if(age >= 21) {
     console.log("Welcome to The Blackjack Game.")
 }else{
@@ -12,30 +14,33 @@ if(age >= 21) {
 }
 
 
-// Picking cards 
+// Method to pick a random card
 function pickCard() {
     return Math.floor( ( Math.random()*(11 - 2 + 1) ) +2 );
 }
 
 
-let firstCard = pickCard();
-console.log("Your first card:", firstCard)
-let secondCard = pickCard();
-console.log("Your second card:", secondCard)
-let sumOfCards = firstCard + secondCard;
-console.log("Your total card value:", sumOfCards)
+// Method to start the game
+function startGame() {
 
+    let firstCard = pickCard();
+    let secondCard = pickCard();
+    let sumOfCards = firstCard + secondCard;
+    console.log("Your total card value:", sumOfCards)
+    console.log("Your second card:", secondCard)
 
-// Checking score
-if(sumOfCards <= 20) {
+    if(sumOfCards <= 20) {
+    // Checking score
     message = "You want to draw another card?";
-}else if(sumOfCards === 21) {
+
+    }else if(sumOfCards === 21) {
     hasBlackjack = true;
     message = "Blackjack! You won 1.5x you bet value.";
-}else {
+    }else {
     isAlive = false;
     message = "Alas, you bust. You lost your money to the dealer.";
-}
+    }
 
-// Final message
-console.log(message);
+    // Final message
+    console.log(message);
+}
